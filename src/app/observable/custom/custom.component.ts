925b9967cb46f9bb92528c538bb0fcd3404190b2
gauravkumar34 <urwidgaurav@gmail.com>
2021-11-21 02:25:22 +0530

    3 files modified

--- src/app/observable/custom/custom.component.ts
+++ src/app/observable/custom/custom.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DesignUtilitesService } from '../../../AppServies/design-utilites.service';
 
 @Component({
   selector: 'app-custom',
   templateUrl: './custom.component.html',
-  styleUrls: ['./custom.component.css']
+  styleUrls: ['./custom.component.css'],
 })
 export class CustomComponent implements OnInit {
-
-  constructor() { }
+  constructor(private _designUl: DesignUtilitesService) {}
 
   ngOnInit() {
-  }
+    //Example 1 Manual
 
+    const cusObs1 = Observable.create((observer) => {
+      setTimeout(() => {
+        observer.next('Angular');
+      }, 1000);
+      setTimeout(() => {
+        observer.next('HTML');
+      }, 3000);
+      setTimeout(() => {
+        observer.next('CSS');
+        observer.complete();
+      }, 5000);
+      setTimeout(() => {
+        observer.next('JavaScript');
+      }, 7000);
+      setTimeout(() => {
+        observer.next('TypeScrpit');
+        observer.error(new Error('Limit Excied'));
+      }, 10000);
+    });
+    cusObs1.subscribe((res) => {
+      this._designUl.print(res);
+      console.log(res);
+    });
+  }
 }
