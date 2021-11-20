2a1d3d695a0f1fbde1e1bee984afb22dbcac7f47
gauravkumar34 <urwidgaurav@gmail.com>
2021-11-21 01:31:49 +0530

    2 files modified

--- src/app/observable/of-form/of-form.component.ts
+++ src/app/observable/of-form/of-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { of } from 'rxjs';
+import { from, of } from 'rxjs';
 import { DesignUtilitesService } from '../../../AppServies/design-utilites.service';
 
 @Component({
@@ -9,11 +9,39 @@ import { DesignUtilitesService } from '../../../AppServies/design-utilites.servi
 })
 export class OfFormComponent implements OnInit {
   constructor(private _designUl: DesignUtilitesService) {}
-
+  ObsMsg: any;
   ngOnInit(): void {
     const Obs1 = of('Anup', 'Shekar', 'Sharma');
     Obs1.subscribe((res) => {
       this._designUl.print(res, 'idContainer');
+    });
+
+    const Obs2 = of({ a: 'Anup', b: 'Shekar', c: 'Sharma' });
+    Obs2.subscribe((res) => {
+      this.ObsMsg = res;
+      console.log(this.ObsMsg);
+    });
+
+    //from
+    const Obs3 = from(['Anup', 'Shekar', 'Sharma']);
+    Obs3.subscribe((res) => {
+      this._designUl.print(res, 'idContainer3');
+    });
+
+    //from -Promise
+    const promise = new Promise((res) => {
+      setTimeout(() => {
+        res('Promise resolve');
+      }, 3000);
+    });
+    const Obs4 = from(promise);
+    Obs4.subscribe((res) => {
+      this._designUl.print(res, 'idContainer4');
+    });
+    //from -String
+    const Obs5 = from('Welcome to my page');
+    Obs5.subscribe((res) => {
+      this._designUl.print(res, 'idContainer5');
     });
   }
 }
