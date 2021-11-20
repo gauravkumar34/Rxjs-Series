d73cce35a65372657157291ad5cc61e53a2d691f
gauravkumar34 <urwidgaurav@gmail.com>
2021-11-21 01:03:37 +0530

    1 file modified, 1 added

--- src/app/observable/of-form/of-form.component.ts
+++ src/app/observable/of-form/of-form.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { DesignUtilitesService } from '../../../AppServies/design-utilites.service';
 
 @Component({
   selector: 'app-of-form',
   templateUrl: './of-form.component.html',
-  styleUrls: ['./of-form.component.css']
+  styleUrls: ['./of-form.component.css'],
 })
 export class OfFormComponent implements OnInit {
+  constructor(private _designUl: DesignUtilitesService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    const Obs1 = of('Anup', 'Shekar', 'Sharma');
+    Obs1.subscribe((res) => {
+      this._designUl.print(res, 'idContainer');
+    });
   }
-
 }
