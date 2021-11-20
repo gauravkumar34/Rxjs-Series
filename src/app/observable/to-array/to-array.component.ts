7b2238ae1df5d9e1bddc2e1f1492e42cc3f69621
gauravkumar34 <urwidgaurav@gmail.com>
2021-11-21 01:56:23 +0530

    2 files modified, 1 added

--- src/app/observable/to-array/to-array.component.ts
+++ src/app/observable/to-array/to-array.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { from, interval, of, Subscription } from 'rxjs';
+import { take, toArray } from 'rxjs/operators';
+import { DesignUtilitesService } from '../../../AppServies/design-utilites.service';
 
 @Component({
   selector: 'app-to-array',
   templateUrl: './to-array.component.html',
-  styleUrls: ['./to-array.component.css']
+  styleUrls: ['./to-array.component.css'],
 })
 export class ToArrayComponent implements OnInit {
+  constructor(private _designUl: DesignUtilitesService) {}
+  users = [
+    { name: 'Gaurav', skills: 'Angular' },
+    { name: 'Saurav', skills: 'Html, css' },
+    { name: 'Rohit', skills: 'JavaScript' },
+  ];
+  sourceSub: Subscription;
+  ngOnInit(): void {
+    //Ex-01
+    const source = interval(1000);
+    this.sourceSub = source.pipe(take(5), toArray()).subscribe((res) => {
+      console.log(res);
+    });
 
-  constructor() { }
+    //Ex-02
+    const source2 = from(this.users);
+    source2.pipe(toArray()).subscribe((res) => {
+      console.log(res);
+      this._designUl.print(res,'idContainer')
+    });
 
-  ngOnInit() {
+    //Ex-03
+    const source3 = of('Anup', 'Shankar', 'Sharma');
+    source3.pipe(toArray()).subscribe((res) => {
+      console.log(res);
+    });
   }
-
 }
