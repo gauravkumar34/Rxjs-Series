b7401a9e32be5d0554c76ef9939b7272af4a3a84
gauravkumar34 <urwidgaurav@gmail.com>
2021-11-25 01:35:35 +0530

    2 files modified

--- src/app/observable/retry/retry.component.ts
+++ src/app/observable/retry/retry.component.ts
@@ -1,15 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-retry',
   templateUrl: './retry.component.html',
-  styleUrls: ['./retry.component.css']
+  styleUrls: ['./retry.component.css'],
 })
 export class RetryComponent implements OnInit {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.fetchData();
   }
-
+  fetchData() {
+    this.http
+      .get('https://global-1bb0f.firebaseio.com/user.json')
+      .subscribe((res) => console.log(res));
+  }
 }
