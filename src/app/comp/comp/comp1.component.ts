43496754f0419afdfe85988a99c1d6156ad7ad69
gauravkumar34 <urwidgaurav@gmail.com>
2021-12-04 15:34:27 +0530

    3 files added

---
+++ src/app/comp/comp/comp1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp/comp1',
+  templateUrl: './comp/comp1.component.html',
+  styleUrls: ['./comp/comp1.component.css']
+})
+export class Comp/comp1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
